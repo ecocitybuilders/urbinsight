bc9bc3bb1220be8f203ccc01171522a5ebc388d1
shkfnly <ashoka.finley@gmail.com>
2016-05-08 18:27:26 -0700

    Add Shower/Toilet and Remove Shower/Toilet now function need to update the mongoose model to reflect the changes + add form submittal + saving

--- src/layouts/DataInputLayout/DataInputPanes/UMIS/WorkbookPanes/water/demandJunctions.js
+++ src/layouts/DataInputLayout/DataInputPanes/UMIS/WorkbookPanes/water/demandJunctions.js
@@ -2,21 +2,64 @@ import React from 'react'
 import { Input, Button } from 'react-bootstrap'
 
 class WaterDemandJunctions extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      toilets: [1],
+      showers: [1]
+    }
+    this.addToilet = this.addToilet.bind(this)
+    this.addShower = this.addShower.bind(this)
+    this.removeToilet = this.removeToilet.bind(this)
+    this.removeShower = this.removeShower.bind(this)
+  }
+  addToilet () {
+    let newAmount = this.state.toilets
+    newAmount.push(newAmount.length + 1)
+    return this.setState({toilets: newAmount})
+  }
+  removeToilet () {
+    let newAmount = this.state.toilets
+    newAmount.pop()
+    return this.setState({toilets: newAmount})
+  }
+  addShower () {
+    let newAmount = this.state.showers
+    newAmount.push(newAmount.length + 1)
+    return this.setState({showers: newAmount})
+  }
+  removeShower () {
+    let newAmount = this.state.showers
+    newAmount.pop()
+    return this.setState({showers: newAmount})
+  }
   render () {
+    let toilets = this.state.toilets.map(function (value) {
+      let refString = 'toilets.activeToilets.' + value + '.flushVolume'
+      return <Input label={'Toilet ' + value + ' Flush Volume:'} type='number' ref={refString} />
+    })
+    let showers = this.state.showers.map(function (value) {
+      let refString = 'showers.activeShowers.' + value + '.flowVolume'
+      return <Input label={'Shower ' + value + ' Flow (L/min):'} type='number' ref={refString} />
+    })
     return (
       <div>
         <h4>Demand Junctions</h4>
           {/* Add functionaility to add a toilet*/}
         <h5>Toilets - Flush Volume</h5>
-        <Button><span className='glyphicon glyphicon-plus'></span></Button>
-        <Input label='Flush Volume:' type='number' ref='toilets.activeToilets.flushVolume'/><br />
+        <Button onClick={this.addToilet}><span className='glyphicon glyphicon-plus'></span>Add Toilet</Button>
+        <Button onClick={this.removeToilet}><span className='glyphicon glyphicon-minus'></span>Remove Toilet</Button>
+        {toilets}
         <Input label='Number of Persons Using Toilets:' type='number' ref='toilets.numPersonsUsingToilets'/><br />
         <Input label='Daily Usage Per Person:' type='number' ref='toilets.dailyPerPersonUsage'/><br />
         {/* <!-- Add functionality to add a shower -->*/}
         <h5>Hygiene</h5>
+        <Button onClick={this.addShower}><span className='glyphicon glyphicon-plus'></span>Add Shower</Button>
+        <Button onClick={this.removeShower}><span className='glyphicon glyphicon-minus'></span>Remove Shower</Button>
+        {showers}
         <Input label='Shower Flow (L/min)' type='number' ref='hygiene.activeShowers.showerA' /><br />
         <Input label='Typical Shower Duration' type='number' ref='hygiene.typicalShowerDuration' /><br />
-        {/* <!-- add funtionality to add a batch -->*/}
+        {/* <!-- add funtionality to add a bath -->*/}
         <Input label='Weekly Showers Per Person' type='number' ref='hygiene.weeklyShowersPerPerson' /><br />
         <Input label='Bath Volume' type='number' ref='hygiene.bathVolume' /><br />
         <Input label='Baths Per Weeks' type='number' ref='hygiene.bathsPerWeek' /><br />
