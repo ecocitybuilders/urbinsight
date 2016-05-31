765b92c0eb8939730fe671d5d0f2bb1e54b9eb94
shkfnly <ashoka.finley@gmail.com>
2016-05-31 13:06:26 -0700

    Finished LayerLink

--- src/components/layerLink.js
+++ src/components/layerLink.js
@@ -11,31 +11,5 @@ LayerLink.propTypes = {
   title: PropTypes.string,
   name: PropTypes.string
 }
-// type Props2 = {
-//   layerSelected: PropTypes.func,
-//   title: PropTypes.string,
-//   name: PropTypes.string
-// }
-// class LayerLink extends React.Component {
-//   props: Props2;
-//   constructor () {
-//     super()
-//     this.state = {
-//       'selected': false
-//     }
-//     this.layerSelected = this.layerSelected.bind(this)
-//   }
-//   layerSelected () {
-//     this.setState({selected: !this.state.selected})
-//     this.props.layerSelected(this.props.name)
-//   }
-//   render () {
-//     let linkClass = classNames({
-//       'selected-layer': this.state.selected
-//     })
-//     return (
-//       <Input label={this.props.title} type='checkbox' onClick={this.layerSelected} className={linkClass} />
-//     )
-//   }
-// }
+
 export default LayerLink
