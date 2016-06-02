f837816963e8c91bdabfae6ed3c9ad804ab439c6
shkfnly <ashoka.finley@gmail.com>
2016-06-02 00:31:36 -0700

    Added new form control functionality

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react'
-import { Modal, Button, Input } from 'react-bootstrap'
+import { Modal, Button, Input, FormGroup, ControlLabel, FormControl, HelpBlock } from 'react-bootstrap'
+// const { Feedback } = FormControl
 import Logo from 'static/images/urbinsight_logo_v1.png'
 // import { EmailSignInForm } from 'redux-auth/bootstrap-theme'
 type Props = {
@@ -15,11 +16,17 @@ export class Login extends React.Component {
     super(props)
     this.state = {
       showModal: props.modalStatus,
-      authToggle: 'login'
+      authToggle: 'login',
+      emailValue: '',
+      passwordValue: '',
+      confirmValue: ''
     }
     this.close = this.close.bind(this)
     this.handleLoginClick = this.handleLoginClick.bind(this)
     this.handleSignUpClick = this.handleSignUpClick.bind(this)
+    // this.getEmailValidationState = this.getEmailValidationState.bind(this)
+    this.getPasswordValidationState = this.getPasswordValidationState.bind(this)
+    this.getPasswordConfirmValidationState = this.getPasswordConfirmValidationState.bind(this)
   }
   close () {
     this.props.statusChange()
@@ -52,6 +59,20 @@ export class Login extends React.Component {
       this.props.onSignUpClick(creds)
     }
   }
+  getValidationState () {
+    return 'success'
+  }
+  getPasswordValidationState () {
+    return 'warning'
+  }
+  getPasswordConfirmValidationState () {
+    return 'warning'
+  }
+  handleChange (e, type) {
+    var obj = {}
+    obj[type] = e.target.value
+    this.setState(obj)
+  }
   render () {
     return (
       <div>
@@ -64,13 +85,54 @@ export class Login extends React.Component {
           <Modal.Body>
             <h3 style={{ display: this.state.authToggle === 'login' ? 'inline' : 'none' }}>Login</h3>
             <h3 style={{ display: this.state.authToggle === 'signup' ? 'inline' : 'none' }}>Sign Up</h3>
+          {/* <form>
+              <FormGroup
+                controlId='email'
+                validationState={this.getEmailValidationState()}
+              >
+                <ControlLabel>Email</ControlLabel>
+                <FormControl
+                  type='email'
+                  value={this.state.emailValue}
+                  placeholder='Email'
+                  onChange={() => this.handleChange('emailValue')}
+                />
+                <FormControl.Feedback />
+              </FormGroup>
+              <FormGroup
+                controlId='password'
+                validationState={this.getPasswordValidationState()}
+              >
+                <ControlLabel>Password</ControlLabel>
+                <FormControl
+                  type='password'
+                  value={this.state.emailValue}
+                  placeholder='Password'
+                  onChange={() => this.handleChange('passwordValue')}
+                />
+                <FormControl.Feedback />
+              </FormGroup>
+              <FormGroup
+                controlId='confirm{assword'
+                validationState={this.getPasswordConfirmValidationState()}
+                style={{ display: this.state.authToggle === 'signup' ? 'inherit' : 'none' }}
+              >
+                <ControlLabel>Confirm Password</ControlLabel>
+                <FormControl
+                  type='password'
+                  value={this.state.emailValue}
+                  placeholder='Confrim Password'
+                  onChange={() => this.handleChange('confirmValue')}
+                />
+                <FormControl.Feedback />
+              </FormGroup>
+            </form>*/}
             <form style={{'marginTop': '15px'}}>
               <Input type='email' ref='username' placeholder='Email'/>
               <Input type='password' ref='password' placeholder='Password' />
               <Input type='password' ref='confirm-password' placeholder='Confirm Password'
                 style={{ display: this.state.authToggle === 'signup' ? 'inline' : 'none' }}/>
             </form>
-            {/* <EmailSignInForm />*/}
           </Modal.Body>
           <Modal.Footer id='login-modal-footer'>
             <div className='login-button-helper'>
