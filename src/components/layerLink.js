fb080b0c20c1b4452a64613f6c8403ba4a68a510
shkfnly <ashoka.finley@gmail.com>
2016-06-04 17:39:17 -0700

    fixed checkbox titles

--- src/components/layerLink.js
+++ src/components/layerLink.js
@@ -2,9 +2,9 @@ import React, { PropTypes } from 'react'
 import { Checkbox } from 'react-bootstrap'
 
 const LayerLink = (props) => <div>
-  <Checkbox label={props.title} type='checkbox'
+  <Checkbox type='checkbox'
     onClick={() => props.layerSelected(props.name)}
-  />
+  >{props.title}</Checkbox>
 </div>
 LayerLink.propTypes = {
   layerSelected: PropTypes.func,
